c9b5ad87cf26592d7d826aa5eb662dd364add7c5
Ana Laf <zanlamar@gmail.com>
2025-10-28 19:56:11 +0100

    logo img compressed

--- EntryPoint/src/app/header/header.ts
+++ EntryPoint/src/app/header/header.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
   standalone: true,
 })
 export class Header {
-
+  authService = inject(AuthService);
+  
+  onLogout() {
+    this.authService.logout();
+  }
 }
