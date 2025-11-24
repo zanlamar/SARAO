7b83381b06001059a746220059ae6429b74c22db
Ana Laf <zanlamar@gmail.com>
2025-11-24 08:06:15 +0100

    auth guard bug fixed

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,17 +1,21 @@
 import { inject } from "@angular/core";
 import { AuthService } from "../services/auth.service"
 import { Router } from "@angular/router";
+
 export const authGuard = (route: any, state: any) => {
     const authService = inject(AuthService);
     const router = inject(Router);
-    if (authService.isAuthenticated()) {
-        return true;
-    } else {
-        const returnUrl = state.url || '/home';
+
+    return authService.waitForAuthentication().then(() => {
+        if (authService.isAuthenticated()) {
+            return true;
+        } else {
+        const returnUrl = state.url || '/calendar-view';
         router.navigate(['/login'], {
             queryParams: { returnUrl: returnUrl }
         });
         return false;
-    }
+        }
+    });
 }
 
