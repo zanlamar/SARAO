7eb3a61f26b6ad66020a8502b31c878facba125c
Ana Laf <zanlamar@gmail.com>
2025-11-30 20:13:21 +0100

    new prep deploy

--- envreader.js
+++ envreader.js
@@ -4,7 +4,7 @@ const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
 const dotenv = require('dotenv').config({path: 'src/.env'}); ;
 
-const firebaseConfig = `export const firebaseConfig = {
+const environment = `export const firebaseConfig = {
     firebaseConfig: {
         apiKey: '${process.env.apiKey}',
         authDomain: '${process.env.authDomain}',
@@ -14,48 +14,27 @@ const firebaseConfig = `export const firebaseConfig = {
         appId: '${process.env.appId}',
         measurementId: '${process.env.measurementId}',
     }
-};
-
-`;
-const environment = `export const environment = {
+}
+    
+export const environment = {
     supabase: {
         url: '${process.env.url}',
         anonKey: '${process.env.anonKey}',
     }
-};
+}
 
-`;
-const storage = `export const storage = {
+export const storage = {
     supabaseUrl: '${process.env.supabaseUrl}',
 };
-
 `;
 
 const targetPath = path.join(__dirname, './src/environments/environment.ts');
 
-fs.appendFile(targetPath, firebaseConfig, (err) => {
-    if (err) {
-        console.error(err);
-        throw err;
-    } else {
-        console.log(successColor, `${checkSign} Successfully generated firebaseConfig environment.ts`);
-    }
-});
-
-fs.appendFile(targetPath, environment, (err) => {
+fs.writeFile(targetPath, environment, (err) => {
     if (err) {
         console.error(err);
         throw err;
     } else {
         console.log(successColor, `${checkSign} Successfully generated environment environment.ts`);
-    }
-});
-
-fs.appendFile(targetPath, storage, (err) => {
-    if (err) {
-        console.error(err);
-        throw err;
-    } else {
-        console.log(successColor, `${checkSign} Successfully generated storage environment.ts`);
     }
 });
